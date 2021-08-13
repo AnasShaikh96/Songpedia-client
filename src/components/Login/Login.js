403e579c8a047d13c45b7548d38b2ddc0471e944
Anas Shaikh <shaikhanas246@gmail.com>
2021-08-13 20:30:37 +0530

    added dashboard components on Homepage

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Login() {
+  return (
+    <div className="container justify-contents-center align-items-center m-3">
+      <a href="http://localhost:8888" className="btn btn-success">
+        login to spotify
+      </a>
+    </div>
+  );
+}
+
+export default Login;
