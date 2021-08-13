f7c05cb01d4610693147c741754260d0b5e852c0
Anas Shaikh <shaikhanas246@gmail.com>
2021-08-13 20:29:52 +0530

    added toplists songs on dashboard

---
+++ src/components/Dashboard/FeaturedArtist.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FeaturedArtist() {
+  return (
+    <div className="py-2" style={{ background: "#eee" }}>
+      <h2>Featured Artist</h2>
+    </div>
+  );
+}
+
+export default FeaturedArtist;
---
+++ src/components/Dashboard/TopList.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+function TopList({ toplist }) {
+  return (
+    <div>
+      <div
+        className="p-2 border-bottom d-flex align-items-center justify-content-between"
+        style={{ background: "#eee" }}
+      >
+        <h2>Top Hindi Songs</h2>
+        <FontAwesomeIcon icon={faArrowRight} className="fs-4" />
+      </div>
+      <div
+        className="d-flex flex-row flex-nowrap my-2 "
+        style={{ overflow: "hidden" }}
+      >
+        {toplist.map((data) => {
+          return (
+            <div
+              className="card m-1"
+              style={{
+                height: "14rem",
+                overflow: "hidden",
+                minWidth: "9rem",
+              }}
+              key={data.trackUri}
+            >
+              <img className="card-img-top" src={data.image} alt="..." />
+              <div className="px-2">
+                <h5 className="text-nowrap" style={{ fontSize: "0.9em" }}>
+                  {data.songName}
+                </h5>
+                <p
+                  className="text-muted text-wrap"
+                  style={{ fontSize: "0.9em" }}
+                >
+                  {data.artistName}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TopList;
