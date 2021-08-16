93c980b7dc0dcb181d437d77cbd210bdd2aa5c62
Anas Shaikh <shaikhanas246@gmail.com>
2021-08-16 18:55:13 +0530

    started adding featuredArtists list

--- src/components/Dashboard/FeaturedArtist.js
+++ src/components/Dashboard/FeaturedArtist.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import GetFeaturedArtist from "../../methods/Actions/getFeaturedArtist";
 
 function FeaturedArtist() {
+  const artists = GetFeaturedArtist();
   return (
-    <div className="py-2" style={{ background: "#eee" }}>
-      <h2>Featured Artist</h2>
+    <div>
+      <div
+        className="p-2 border-bottom d-flex align-items-center justify-content-between"
+        style={{ background: "#eee" }}
+      >
+        <h2>Featured Artists</h2>
+        <FontAwesomeIcon icon={faArrowRight} className="fs-4" />
+      </div>
+      <div
+        className="d-flex flex-row flex-nowrap my-2 "
+        style={{ overflow: "hidden" }}
+      >
+        idhar aayega
+        {artists.map((artist) => {
+          return (
+            <div>
+              <p>{artist.songName} </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
